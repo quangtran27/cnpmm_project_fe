28c8e643ddf5a215874c170ad435f014aa0e8168
Quang Tran <quangtv2702@gmail.com>
2023-12-05 17:51:47 +0700

    Fix auth page ui

--- src/pages/forgot-password.tsx
+++ src/pages/forgot-password.tsx
@@ -36,7 +36,7 @@ export default function ForgotPassword() {
 
   return (
     <section className='min-h-screen bg-gray-100 bg-auth bg-cover bg-no-repeat pt-1'>
-      <div className='mx-auto mt-[50%] flex flex-col items-center justify-center px-6 py-8 lg:py-0'>
+      <div className='mx-auto flex h-screen flex-col items-center justify-center px-6 py-8 lg:py-0'>
         <div className='w-full rounded-2xl border bg-white shadow-sm sm:max-w-md md:mt-0 xl:p-0'>
           <Link to='/' className='mt-6 flex items-center justify-center text-2xl font-semibold'>
             <img className='mr-2 h-10' src='/images/logo.png' alt='logo' />
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -45,7 +45,7 @@ export default function Login() {
 
   return (
     <section className='min-h-screen bg-gray-100 bg-auth bg-cover bg-no-repeat pt-1 text-sm'>
-      <div className='mx-auto mt-[18%] flex flex-col items-center justify-center px-6 py-8 lg:py-0'>
+      <div className='mx-auto flex h-screen flex-col items-center justify-center px-6 py-8 lg:py-0'>
         <div className='w-full rounded-2xl border bg-white shadow-sm sm:max-w-md md:mt-0 xl:p-0'>
           <Link to='/' className='mt-6 flex items-center justify-center text-2xl font-semibold'>
             <img className='mr-2 h-10' src='/images/logo.png' alt='logo' />
